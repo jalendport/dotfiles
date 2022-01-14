39c6d067c3f8857231172191c1eb297256b97ba7
Jalen Davenport <jalen@jalendport.com>
2022-01-13 22:27:34 -0500

    Add Powerlevel10k

---
+++ zsh/prompt.zsh
@@ -0,0 +1,9 @@
+# Enable Powerlevel10k instant prompt - should stay close to the top of .zshrc
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+source ~/.dotfiles/zsh/plugins/powerlevel10k/powerlevel10k.zsh-theme
+
+# To customize prompt, run `p10k configure` or edit p10k.zsh
+[[ ! -f ~/.dotfiles/zsh/p10k.zsh ]] || source ~/.dotfiles/zsh/p10k.zsh
